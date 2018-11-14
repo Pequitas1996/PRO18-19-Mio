b811f3f5ea2c4a7450f208b3019dfb5709848bb2
Pequitas <Pequitas@LAPTOP-GKES1CGC>
2018-11-14 08:13:22 +0000

    Ejercicios de clase de comparar cadeas y de ordenar numeros

---
+++ src/ejercicios/Ejercicios.java
@@ -0,0 +1,448 @@
+package ejercicios;
+
+import java.util.Random;
+
+import modelo.Persona;
+
+public class Ejercicios {
+	public void pruebasAPI() { /*--------04/10/2018-----------*/
+		/*
+		 * 1. Imprime por consola el valor de 2 elevado a 3 2. Imprime por consola un
+		 * valor aleatorio entre 1 y 100. 3. En la cadena "LAS PALMAS DE GRAN CANARIA",
+		 * imprime "GRAN"
+		 */
+
+		// 1. Usa la clase estatica Math y su método pow
+
+		// System.out.println("el resultado del ejercicio 1 es: " + Math.pow(2.0f,
+		// 3.0f));
+
+		// 2. Usa la clase java.util.Ramdom y el método rnd
+
+		//Random rnd = new Random();
+		// System.out.println("el resultado del ejercicio 2 es: " + rnd.nextInt(100));
+
+		// 3. Usar el método substring de la clase java.lang.String
+
+		// System.out.println("el resultado del ejercicio 3 es: " + "LAS PALMAS DE GRAN
+		// CANARIA".substring(14, 18));
+	}
+
+	/*-----------------------09/10/2018--------------------------*/
+	/* Mostrar por consola los numeros comprendidos entre dos enteros a y b */
+
+	// 1. prototipo, cabezera
+
+	public void listaIntervaloEnteros(int primero, int ultimo) {
+		// 2. cuerpo, implementacion del método
+		int i = primero;
+		/*
+		 * while(i <= ultimo) { System.out.println(i); i++; //incrementador. }
+		 */
+
+		for (int j = primero; j <= ultimo; j++)
+			System.out.println(j);
+
+	}
+
+	/*-----------------------------11/10/2018---------------------*/
+	/* Actividad sobre las series fibonnaci. */
+
+	public static void serieFibonnaci(int cuantos) {
+		int num1 = 0;
+		int num2 = 1;
+		int total = 1;
+		System.out.print(num1 + ", " + num2);
+		for (int i = 0; i < cuantos; i++) {
+			total = num1 + num2;
+			System.out.print("," + total);
+			num1 = num2;
+			num2 = total;
+		}
+		System.out.println();
+	}
+
+	/*-----------------------11/10/2018-------------------------------*/
+	/*-------------------Actividad para casa-------------------------*/
+	/* 1)Devolver el numero menor de tres numeros */
+
+	public int calculaMenor(int x, int y, int z) {
+		if (x < y)
+			if (x < z)
+				return x;
+			else
+				return z;
+		else if (y < z)
+			return y;
+		else
+			return z;
+
+	}
+
+	/*-----------------------17/10/2018--------------------------*/
+
+	/* 1) Hacer una version del metodo "calcNumMenor que devuelva el menor de 4
+	 * números.*/
+
+	public int calculaMenor(int x, int y, int z, int w) {
+		if (x < y) // condicion que indica si x es menor que y;
+			if (x < z)// condicion que indica si x es menor que z;
+				if (x < w) // condicion que indica si x es menor que w;
+					return x; // si es menor que w se devuelve al numero x;
+				else // si no
+					return w; // devuelve el numero w
+			// se cierra el primer bucle en donde se conoce si x < w;
+			else if (z < w) // si es distinto a x salta a buscar si z es menor que w
+				return z;// devuelve el numero z;
+			else // si no
+				return w; // devuelve el numero z;
+			//Se cierra el segundo bucle donde se conoce si z < w;
+		else // si no
+			// aqui se abren otros bucles primero para averiguar si y es menor que los otros parametros;
+			if(y < z)//condicion que indica si y es menor que z;
+				if(y < w) //condicion en donde averiguamos si y es menor que w;
+					return y; //devuelve el numero y;
+				else // si no 
+					return w; //duelve el numero w;
+				//se cierra el primer bucle en donde se conoce si y < w
+			else if(z < w) // se vuelve a preguntar en otro bucle si z es menos que w
+				return z; //devuelve el numero z
+			else // si no 
+				return w; //devuelve el numero w;
+			//se cierra el segundo bucle donde se conoce que z < w
+	//se cierra el primero de todos los bucles en donde se compara que x < y.
+	}
+	
+	/*2) imprime por consola n números aleatorios enteros aleatorios entre 1 y 100*/
+	
+	public void imprimeAleatorios(int n) { //n es el numero de veces que se genera los aleatorios
+		Random rnd = new Random();
+		for (int i = 0; i < n; i++) {
+			int numero = rnd.nextInt(6);
+			System.out.println(i + 1 +  ".-" + numero);
+			
+		}
+	}
+	
+	/*3) Genera aleatorios que los guarde en un array */
+	public int[] generaAleatorios(int cuantos, int inferior, int superior) { //parametro a usar
+		Random rnd = new Random();
+		int[] resultado = new int[cuantos];
+		//creamos una variable donde se guardan los resultados dentro del array
+		for (int i = 0; i < cuantos; i++) {
+			//System.out.println(inferior + rnd.nextInt(superior  - inferior + 1));
+			resultado[i] = inferior + rnd.nextInt(superior - inferior + 1);	
+		}
+		return resultado;
+
+	}
+	
+	/*------------------------------18/10/2018----------------------*/
+	/* 1) imprime por consola n números aleatorios enteros aleatorios entre inferior
+	 * y superior
+	 */
+
+	public void imprimeAleatorios(int n, int inferior, int superior) { // n, cuantos números
+		Random rnd = new Random();
+		for (int i = 0; i < n; i++) {
+			int numero = inferior + rnd.nextInt(superior - inferior + 1);
+			System.out.println(i + 1 + ".- " + numero);
+		}
+
+	}
+
+	/* 2)Generar un metodo que cuando se lanze varias veces un dado o otra cosa se
+	 * guarde las n veces que aparece el mismo numero y que se guarde dentro de un
+	 * array */
+	public int[] generaEstadisticaAparicion(int n, int inferior, int superior) {
+		// metodo de mauricion
+		int[] resultado = new int[superior - inferior + 1];
+		Random rnd = new Random();
+		for (int i = 0; i < n; i++) {
+			int numero = inferior + rnd.nextInt(superior - inferior + 1);
+			resultado[numero - 1]++;
+		}
+		return resultado;
+	}
+
+	public int[] generaEstadisticaAparicion1(int cuantos, int inferior, int superior) {
+		// metodo distinto al de mauricio que funciona generado el año pasado
+		int[] resultado = new int[superior - inferior + 1];
+		int[] aleatorios = this.generaAleatorios(cuantos, inferior, superior);
+		// Random rnd= new Random();
+		for (int i = 0; i < aleatorios.length; i++) {
+
+			resultado[aleatorios[i] - 1]++;
+		}
+		return resultado;
+	}
+
+	/*--------------------------24/10/2018*---------------------------------*/
+
+	/* crear un metodo que devuelva una lista de n personas, se crea las personas
+	 * con el constructor Persona.*/
+
+	public Persona[] listarPersonas(int n) {
+		Persona[] lista = new Persona[n];
+
+		for (int i = 0; i < lista.length; i++) {
+			lista[i] = new Persona();
+
+		}
+
+		return lista;
+	}
+
+	/* crear un metodo que devuelva el saldo de una cuenta, partiendo de una lista
+	 * de movimientos y del saldo inicial*/
+
+	public float saldoDeUnaCuenta(float saldoInicial, float[] movimientos) {
+		float saldoFinal = saldoInicial;
+		for (int i = 0; i < movimientos.length; i++)
+			saldoFinal += movimientos[i];
+		return saldoFinal;
+	}
+
+	/*--------------------------25/10/2018-----------------------------*/
+	public void pruebaCadena() {
+
+		String nombre = "LAS PALMAS DE GRAN CANARIA";// declaramos el objeto tipo String
+		// iterar en la cadena mostrando todos sus caracteres
+		for (int i = 0; i < nombre.length(); i++) {
+			System.out.println(nombre.charAt(i));
+		}
+	}
+
+	/*------------------Comparar cadenas---------------*/
+	public void compararCadenas() {
+		String cad1 = "abca";
+		String cad2 = "abcdaaaaaaaaaaaa";
+		// System.out.println(cad1.compareTo(cad2));
+		System.out.println(cad2.compareToIgnoreCase(cad1));
+
+	}
+
+	/*------------------31/10/201------------------------*/
+	/*Empezamos a trabajar con matrices en donde una matriz es un array de arrays
+	 * en donde se explica como crear matrices con arrays */
+
+	/* Crear un metodo que recorra la matriz de y recoga el acumulado de cada
+	 * vendedor y lo sume y tener un resumen */
+
+	public float[] resumenVentasPorVendedor(float[][] ventas) {
+		float[] resultado = new float[ventas.length];
+		float acumulador = 0;
+		// iterar por filas/columnas acumulado ventas de cada año
+
+		for (int i = 0; i < ventas.length; i++) {
+			acumulador = 0;
+			for (int j = 0; j < ventas[i].length; j++)
+				acumulador += ventas[i][j];
+			resultado[i] = acumulador;
+		}
+		return resultado;
+	}
+
+	/*-------------------------06/11/2018--------------------*/
+	/* Actividad de devolver numeros de una cadena de numeros y si no se puede que
+	 * nos devuelva un -1 */
+
+	public int[] convierteCadenas(String[] cadenaNumerica) {
+		// el Array de salida tiene el mismo numero de elementos que el de entrada
+
+		// si un numero no es valido, se pone -1
+		int[] resultado = new int[cadenaNumerica.length];
+		for (int i = 0; i < resultado.length; i++)
+			try {
+				resultado[i] = Integer.parseInt(cadenaNumerica[i]);
+				// System.out.println("LLega aqui");
+
+			} catch (NumberFormatException e) {
+				// numero no valido
+				resultado[i] = -1;
+			}
+
+		return resultado;
+	}
+
+	/*---------------Matriz ventas por mes-------------------*/
+	public float[] resumenVentasPorMes(float[][] ventas) {
+		// floar acumVendedor=0;
+		float[] resultado = new float[ventas.length];
+		// iterar por filas/columnas acumulado ventas por mes de cada año
+		for (int i = 0; i < ventas.length; i++) {
+			// acumVendedor= 0;
+			for (int j = 0; j < ventas[i].length; j++)
+				resultado[i] += ventas[i][j];
+			// resultado[i]= acumVendedor;
+		}
+
+		return resultado;
+	}
+
+	/*-----------------07/11/018----------------------------*/
+	/* Vamos a leer la matriz por columnas y averiguar el resultado de cada mes */
+
+	public float[] resumenVentasdeCadaMes(float[][] resumen) {
+		float[] resultado = new float[resumen[0].length];
+		// float acumVendedor;
+		for (int j = 0; j < resumen[0].length; j++) {
+			// acumVendedor = 0;
+			for (int i = 0; i < resumen.length; i++)
+				// acumVendedor += resumen[i][j]; primera forma de hacerlo con un acumulador a 0
+				// resultado[j]= acumVendedor;
+				resultado[j] += resumen[i][j];
+			// segunda forma de hacerlo en donde el programa te da el acumulado ya en 0
+
+		}
+		return resultado;
+	}
+
+	/* Vamos a crear un bucle para un reloj, en donde generamos 3 for en donde se
+	 * suman las horas, minutos y segundos. */
+
+	public void mostrarReloj() {
+		
+		for (int h = 0; h < 1; h++) {
+			for (int m = 0; m < 60; m++) 
+				for (int s = 0; s < 60 ; s++) {
+					System.out.println(h +":" +  m + ":" + s);
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+		}
+	}
+	
+	/*----------------08/11/2018-------------------------*/
+	/*Creacion de un metodo para averiguar si el numero es primo o no*/
+	
+	public boolean esPrimo(int n) {
+		for (int i = 2; i < n; i++) 
+			if (n % i == 0) 
+				return false;
+			
+		return true;
+		
+	}
+	
+	/*Metodo para hacer un listado de numeros primos*/
+	
+	
+	public void listarPrimos (int desde, int hasta) {
+		for (int i = desde; i < hasta; i++) 
+			if (esPrimo(i)) 
+				System.out.print(i + ", ");
+				
+	}
+	
+	/*Listar primos 2*/
+	public void listarPrimos2 (int desde, int cuantos) {
+		int contador = 0;
+		while (contador < cuantos) 
+			if (esPrimo(desde++) ) { 
+				System.out.print(desde + ", ");
+				contador ++;
+			}
+	}
+	
+	/*Lista primos 3 y que se guarden en una lista*/
+	
+	public int[]listarPrimos3 (int desde, int cuantos) {
+		int[] resultado = new int[cuantos];
+		int contador = 0;
+
+		while (contador < cuantos) 
+			if (esPrimo(desde) ) { 
+				//System.out.print(desde + ", ");
+				resultado[contador] = desde;
+				contador ++;
+			}
+		
+			desde++;
+		return resultado;
+	}
+
+	/*----------------------13/11/2018----------------------*/
+	
+	/* 1)Vamos a desglosar monedas de una cantidad cuantas monedas lleva cada una */
+
+	public int[] desgloseMoneda(int cantidad) {
+		int[] monedas = { 200, 100, 50, 20, 10, 5, 2, 1 };
+		int[] resultado = new int[monedas.length];
+		int indice = 0;
+		// bucle con while
+		while (cantidad % monedas[indice] != 0) { 
+			//dice que cantidad de tanto por ciento de monedas tiene que ser distinto de 0
+
+			// if (cantidad / monedas[indice] > 0) {
+			// cantidad = cantidad % monedas[indice];
+			resultado[indice] = cantidad / monedas[indice];
+			//en donde la cantidad dividido por monedas e indice igualado a 0
+			cantidad = cantidad % monedas[indice];
+			//lo multipilice o sume 
+
+			indice++; //y se sume dentro de la array
+		}
+
+		// Para que cuando el resto sea 0 coja el valor
+		resultado[indice] = cantidad / monedas[indice];
+
+		return resultado;
+	}
+	
+	/*public int[] desgloseMoneda(int cantidad) { //desglose con un bucle for
+		int[] monedas = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+		int[] resultado = new int[monedas.length];
+		int resto = cantidad;
+			for (int i = 0; i < monedas.length; i++) {
+					resultado[i]= resto / monedas[i];
+					resto = resto%monedas[i];
+			}
+			return resultado;
+	}*/
+	
+	/*2) Ahora vamos a crear otro metodo que llame al anterior pero te lo listara 
+	 * por pantalla todas las monedad que se utilizan*/
+	
+	public void listaDesgloseMoneda(int cantidad){
+		System.out.println("Cantidad " + cantidad);
+		int[] monedas = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+		int[] resultado = desgloseMoneda(cantidad);
+		
+		for (int i = 0; i < resultado.length; i++)
+			if (resultado[i] !=0) {
+				System.out.println(monedas[i]  + " -> " + resultado[i]);
+			}
+	}
+	
+	public void ordenaArray(int[] array) {
+		for (int i = 0; i < array.length -1 ; i++) 
+			for (int j = i + 1; j < array.length; j++) 
+				if (array[i] < array[j]) {
+					int aux = array[i];
+					array[i] = array[j];
+					array[j]=  aux;
+					
+				}
+	}
+	
+	/*-----------------------14/11/2018----------------------*/
+	
+	/* 1) ordenar dentro de un array de numeros, ordenar un array de cadenas*/
+	
+	public void ordenaCadena(String[] cadena) {
+		
+		for (int i = 0; i < cadena.length - 1 ; i++)
+			for (int j = i + 1 ; j < cadena.length; j++)
+				if (cadena[i].compareTo(cadena[j]) > 0) {
+					String aux = cadena[i];
+					cadena[i] = cadena[j];
+					cadena[j] = aux;
+					
+				}
+	}
+
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,132 @@
+package principal;
+
+import java.time.LocalDate;
+
+import java.util.Random;
+
+import org.omg.CORBA.PUBLIC_MEMBER;
+
+import modelo.Persona;
+
+import ejercicios.Ejercicios;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		Persona juan = new Persona("42268525F", "Juan", 47);
+		Persona persona1 = new Persona();
+		/*---------------02/10/2018 Parametros con el Constructor--------------------
+		 * El metodo Persona() tiene que ver con el Constructor, en donde le estamos pasando los
+		 * parametros con los que vamos a trabajar */
+		// int pasos = juan.caminar(20);
+
+		/*-----------------------------04/10/2018---------------------------------*/
+		Ejercicios ejer = new Ejercicios();
+
+		// ejer.pruebasAPI();
+
+		/*-----------------------09/10/2018--------------------------*/
+
+		// int x = 10;
+		// int y = 30;
+
+		// ejer.listaIntervaloEnteros(x, y);
+		/*---------------------10/10/2018-----------------------------*/
+		// serieFibonnaci(8);
+
+		/*---------------------11/10/2018------------------------------*/
+		// int a = 3;int b = 50; int c = 10; int d =12;
+		// system.out.println("el menor es : " + ejer.calculaMenor(a, b, c));
+
+		// *---------------------17/10/2018----------------------------*/
+
+		// System.out.println("el menor es : " + ejer.calculaMenor(a, b, c, d));
+		// ejer.imprimeAleatorios(15);
+		int[] numeros = ejer.generaAleatorios(15, 10, 200);
+
+		// int[] estatidisticaDados = ejercicios.generaEstadisticaAparicion(10, 1, 6);
+		// int[] estadistica = ejercicios.generaEstadisticaAparicion1(20, 1, 20);
+
+		/*-----------------------24/10/218----------------------*/
+		Persona[] personas = ejer.listarPersonas(10);
+
+		float saldo = 100.0f;
+		float[] movimientos = { 10.0f, -5.0f, 20.5f };
+
+		// System.out.println("El saldo final es : " +
+		// ejercicios.saldoDeUnaCuenta(saldo, movimientos));
+
+		/*-------------------25/10/2018------------------*/
+		// ejercicios.pruebaCadena();
+		ejer.compararCadenas();
+		// String[] diasSemana = {"LUNES", "MARTES"};
+		// System.out.println("el menor es : " + menor);
+
+		/*-------------------31/10/2018-----------------*/
+
+		float[][] ventaYears = {
+
+				/* hay 3 filas (vendedores) y 12 columnas(meses) */
+				{ 12.5f, 13.50f, 8.5f, 5.0f, 10.5f, 9.5f, 20.5f, 10.5f, 4.0f, 6.5f, 5.5f, 6.0f },
+				{ 10.5f, 15.50f, 3.5f, 2.0f, 11.5f, 9.5f, 2.5f, 10.5f, 4.0f, 6.5f, 5.5f, 6.0f },
+				{ 11.5f, 10.50f, 4.5f, 1.0f, 9.5f, 9.5f, 3.5f, 10.5f, 7.0f, 1.5f, 3.5f, 6.0f } };
+
+		// crear un array que guarde el resumen de cada vendedor
+		float[] resumenVendedor = ejer.resumenVentasPorVendedor(ventaYears);
+
+		/*--------------------06/11/2018------------------*/
+
+		String[] nombresVendedor = { "JUAN CARLOS", "ISABEL", "MARTA" };
+		float[][] ventaYear = {
+
+				/* hay 3 filas (vendedores) y 12 columnas(meses) */
+				{ 12.5f, 13.50f, 8.5f, 5.0f, 10.5f, 9.5f, 20.5f, 10.5f, 4.0f, 6.5f, 5.5f, 6.0f },
+				{ 10.5f, 15.50f, 3.5f, 2.0f, 11.5f, 9.5f, 2.5f, 10.5f, 4.0f, 6.5f, 5.5f, 6.0f },
+				{ 11.5f, 10.50f, 4.5f, 1.0f, 9.5f, 9.5f, 3.5f, 10.5f, 7.0f, 1.5f, 3.5f, 6.0f } };
+
+		float[] ventas = ejer.resumenVentasPorVendedor(ventaYear);
+
+		/*-------------------07/11/2018-----------------------*/
+		float[][] ventaMes = {
+
+				/* hay 3 filas (vendedores) y 12 columnas(meses) */
+				{ 12.5f, 13.50f, 8.5f, 5.0f, 10.5f, 9.5f, 20.5f, 10.5f, 4.0f, 6.5f, 5.5f, 6.0f },
+				{ 10.5f, 15.50f, 3.5f, 2.0f, 11.5f, 9.5f, 2.5f, 10.5f, 4.0f, 6.5f, 5.5f, 6.0f },
+				{ 11.5f, 10.50f, 4.5f, 1.0f, 9.5f, 9.5f, 3.5f, 10.5f, 7.0f, 1.5f, 3.5f, 6.0f } };
+		float[] mes = ejer.resumenVentasdeCadaMes(ventaMes);
+
+		// Metodo muestra reloj
+
+		// ejercicios.mostrarReloj();
+
+		/*----------------08/11/2018-------------------------*/
+
+		// System.out.println(ejercicios.esPrimo(99525));
+		int inicio = 500;
+		int cuantos = 5;
+		ejer.listarPrimos2(inicio, cuantos);
+		System.out.println();
+
+		// int[] numeros = ejer.listarPrimos3(inicio, cuantos);
+		// String[]cadena= {"123", "63A", "101", "8k8", "9811", "xyz"};
+
+		// int[] datos = ejer.convierteCadenas(cadena);
+
+		/*---------------13/11/2018---------------------*/
+
+		// int[] desgloce = ejercicios.desgloseMoneda(13);
+		// ejercicios.listaDesgloseMoneda(1234);
+
+		int[] x = { 78, 34, 1, 34, 54, 29, 3 };
+		ejer.ordenaArray(x);
+
+		/*--------------------14/11/2018--------------------*/
+		String[] cadena = { "perro", "gato", "145", "arroz" };
+		ejer.ordenaCadena(cadena);
+
+		System.out.println("FIN DEL PROGRAMA");
+
+	}
+
+}
